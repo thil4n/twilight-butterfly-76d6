143751dd23443820082db3ceed26e7482d1da5db
Thilan <thil4n@gmail.com>
2025-04-30 16:30:01 +0530

    Remove unused MTLS certificate configurations and clean up wrangler files

--- test.js
+++
@@ -1,6 +0,0 @@
-process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
-
-const apimResponse = await fetch('https://jwt-test.hacksland.net/netty/1/unsecured', {
-	method: 'POST',
-});
-console.log('APIM Response:', apimResponse.body);
